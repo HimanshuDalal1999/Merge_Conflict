140e99dd2a7213939ea4b54a85dfc40783d146ff
Himanshu@Dalal <himanshuapachertr160@gmail.com>
2024-03-17 12:48:13 +0530

    Pussing 1703 1248

---
+++ src/Package/Class01.java
@@ -0,0 +1,11 @@
+package Package;
+
+public class Class01 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Merge Conflict");
+		
+	}
+
+}
---
+++ src/Package/Class02.java
@@ -0,0 +1,11 @@
+package Package;
+
+public class Class02 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Merge Conflict 02");
+		
+	}
+
+}
